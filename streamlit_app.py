c67afb3e1071743d2c9a2e419c620b2d19aab550
Akshay Dharmavaram <30520349+Aks-Dmv@users.noreply.github.com>
2024-07-17 14:08:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ import json
 import os
 import csv
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+# __import__('pysqlite3')
+# import sys
+# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 # import gspread
 # from oauth2client.service_account import ServiceAccountCredentials
