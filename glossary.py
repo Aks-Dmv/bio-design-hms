30793ba44f849abc0772ff045ef76e53acbdb183
Akshay Dharmavaram <30520349+Aks-Dmv@users.noreply.github.com>
2024-07-30 21:36:30 -0400

    Delete glossary.py

--- glossary.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-
-import pandas as pd
-
-st.set_page_config(page_title="Glossary", page_icon="📊")
-
-st.markdown("# Glossary (Coming Soon)")
