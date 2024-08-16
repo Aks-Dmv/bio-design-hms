94f4f1584c361a3861b5225cf0db5daa4b1e0bb0
Akshay Dharmavaram <30520349+Aks-Dmv@users.noreply.github.com>
2024-08-16 00:36:10 -0400

    Update add_new_observation.py

--- pages/add_new_observation.py
+++ pages/add_new_observation.py
@@ -32,10 +32,19 @@ observations_csv = "observations.csv"
 OPENAI_API_KEY = st.secrets["openai_key"]
 
 # Access the credentials from Streamlit secrets
-creds_json = st.secrets["google"]["credentials"]
-
-# Convert JSON string to dictionary
-creds_dict = json.loads(creds_json)
+creds_dict = {
+    "type" : st.secrets["gcp_service_account"]["type"],
+    "project_id" : st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key" : st.secrets["gcp_service_account"]["private_key"],
+    "client_email" : st.secrets["gcp_service_account"]["client_email"],
+    "client_id" : st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"],
+}
 
 
 if 'observation' not in st.session_state:
