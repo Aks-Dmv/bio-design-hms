eda80d2e16d48b12b09308755e3aaddce68765ea
Akshay Dharmavaram <30520349+Aks-Dmv@users.noreply.github.com>
2024-08-18 15:23:06 -0400

    Update ask_observations.py

--- pages/ask_observations.py
+++ pages/ask_observations.py
@@ -11,7 +11,20 @@ from langchain.schema.runnable import RunnableLambda
 from langchain.prompts import PromptTemplate
 from langchain_pinecone import PineconeVectorStore
 
-from add_new_observation import creds_dict
+# Access the credentials from Streamlit secrets
+creds_dict = {
+    "type" : st.secrets["gcp_service_account"]["type"],
+    "project_id" : st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id" : st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key" : st.secrets["gcp_service_account"]["private_key"],
+    "client_email" : st.secrets["gcp_service_account"]["client_email"],
+    "client_id" : st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri" : st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri" : st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url" : st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url" : st.secrets["gcp_service_account"]["client_x509_cert_url"],
+    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"],
+}
 
 # Google Sheets setup
 SCOPE = [
