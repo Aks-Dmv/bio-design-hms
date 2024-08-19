eddfcb13ec990d2bfad4d11d0eab3ce9ae42a4a8
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-08-19 12:03:39 -0400

    Update view_dataset.py

--- pages/view_dataset.py
+++ pages/view_dataset.py
@@ -56,7 +56,7 @@ df = pd.read_csv("observations.csv", delimiter=';')
 st.markdown("""
     <style>
     div.stButton > button {
-        font-size: 800px;
+        font-size: 8px;
         padding: 30px 64px;
         background-color: #365980; /* blueish color */
         color: white;
