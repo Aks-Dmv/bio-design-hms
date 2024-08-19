2f200c17e65a3d1881f9213a81bb103bfb5b5fac
Wazeer Zulfikar <wazeer.zulfikar@gmail.com>
2024-08-19 23:21:07 +0200

    refresh pagev5

--- pages/Record_New_Observation.py
+++ pages/Record_New_Observation.py
@@ -369,6 +369,7 @@ st.markdown(st.session_state['result'], unsafe_allow_html=True)
 if st.session_state['rerun']:
     time.sleep(3)
     clear_observation()
+    st.rerun()
 
 ##########
 if st.button("Add Observation to Team Record", disabled=st.session_state['observation_summary'] == ""):
