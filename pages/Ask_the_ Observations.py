2b238a17b7a73e647d4f9cd70577e5e4deaa5e30
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-08-19 15:15:42 -0400

    Update Ask_the_ Observations.py

--- pages/Ask_the_ Observations.py
+++ pages/Ask_the_ Observations.py
@@ -130,8 +130,8 @@ st.markdown("""
         justify-content: center;
     }
     .big-button {
-        font-size: 25px;
-        padding: 10px 60px;
+        font-size: 22px;
+        padding: 10px 40px;
         background-color: #365980; /* blueish color */
         color: white;
         border: none;
