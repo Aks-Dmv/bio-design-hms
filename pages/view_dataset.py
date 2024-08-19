deaa7a78ee74acfaf80e823cdeb6e3ba6f99dcfb
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-08-19 12:09:46 -0400

    Update view_dataset.py

--- pages/view_dataset.py
+++ pages/view_dataset.py
@@ -79,22 +79,6 @@ df = pd.read_csv("observations.csv", delimiter=';')
 
 ###############
 
-import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
-import pandas as pd
-
-st.set_page_config(page_title="View Observations Dataset", page_icon="📊")
-
-st.markdown("# View Observations Dataset")
-
-# Embedding Google Sheets using a larger iframe
-st.markdown("""
-    <iframe src="https://docs.google.com/spreadsheets/d/1wid5imrlhkXOvmpWCbZzhAUrVSTS5iNOX3JASOxtJM4/edit?usp=sharing"
-    width="100%" height="800px"></iframe>
-    """, unsafe_allow_html=True)
-
-df = pd.read_csv("observations.csv", delimiter=';')
-
 st.markdown("---")
 
 # Add custom CSS for a larger button
