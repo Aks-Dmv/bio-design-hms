17019ff72338babb9310c87e4b28f1855f50eb8a
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-08-19 23:19:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -114,21 +114,6 @@ def main():
 #         st.info("New licenses coming soon!")
 ############3
 
-# if __name__ == "__main__":
-#     st.set_page_config(initial_sidebar_state="collapsed")
-
-#     if "login_status" not in st.session_state:
-#         st.session_state["login_status"] = "not_logged_in"
-
-#     if st.session_state["login_status"] == "success":
-#         # st.query_params(page="main_menu")
-#         # st.query_params[''] = "main_menu"
-#         # # st.page_link("pages/main_menu.py")
-#         # st.rerun()
-#         switch_page("main_menu")
-#     else:
-#         main()
-
 if __name__ == "__main__":
     st.set_page_config(initial_sidebar_state="collapsed")
 
@@ -136,7 +121,22 @@ if __name__ == "__main__":
         st.session_state["login_status"] = "not_logged_in"
 
     if st.session_state["login_status"] == "success":
-        switch_page("Menu")
+        # st.query_params(page="main_menu")
+        # st.query_params[''] = "main_menu"
+        # # st.page_link("pages/main_menu.py")
+        # st.rerun()
+        switch_page("main_menu")
     else:
         main()
+
+# if __name__ == "__main__":
+#     st.set_page_config(initial_sidebar_state="collapsed")
+
+#     if "login_status" not in st.session_state:
+#         st.session_state["login_status"] = "not_logged_in"
+
+#     if st.session_state["login_status"] == "success":
+#         switch_page("Menu")
+#     else:
+#         main()
 
