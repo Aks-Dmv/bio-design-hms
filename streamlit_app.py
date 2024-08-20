e9db1e249646c10a58521f2842c796c079bbe3e4
AnaTraperoM <60890310+AnaTraperoM@users.noreply.github.com>
2024-08-19 21:04:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ def main():
         ('Observation Assistant', 'Need Statement Assistant (Coming Soon)'),
         index=0  # 0 is the index for the default option 'Observation Assistant'
     )
+
+    
     st.write("")
     if st.button("Sign In"):
         st.info("New licenses coming soon!")
@@ -49,22 +51,22 @@ def main():
     #             st.rerun()
     #     st.error("Try again please")
 
-with st.form(key="login_form"):
-    st.write("Login:")
-    username = st.text_input("Username:", key="username")
-    password = st.text_input("Password:", type="password", key="password")
-    stay_logged_in = st.checkbox("Stay logged in")
-
-    submit_button = st.form_submit_button("Submit")
-
-if submit_button:
-    user_list = st.secrets["login-credentials"]
-    for user_dict in user_list:
-        if username == user_dict["username"] and password == user_dict["password"]:
-            st.success("Login successful")
-            st.session_state["login_status"] = "success"
-            st.rerun()
-    st.error("Try again please")
+    with st.form(key="login_form"):
+        st.write("Login:")
+        username = st.text_input("Username:", key="username")
+        password = st.text_input("Password:", type="password", key="password")
+        stay_logged_in = st.checkbox("Stay logged in")
+    
+        submit_button = st.form_submit_button("Submit")
+    
+    if submit_button:
+        user_list = st.secrets["login-credentials"]
+        for user_dict in user_list:
+            if username == user_dict["username"] and password == user_dict["password"]:
+                st.success("Login successful")
+                st.session_state["login_status"] = "success"
+                st.rerun()
+        st.error("Try again please")
 
 # if __name__ == "__main__":
 #     st.set_page_config(initial_sidebar_state="collapsed")
